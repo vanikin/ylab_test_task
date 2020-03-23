68ecb3d2ef1a82955968a9c1a3dd193d6ed519e8
Anikin Vasiliy <anikin.vasya@gmail.com>
2020-03-23 15:25:29 +0300

    Add transactions admin views

--- ylab_test_task/money_transfer/admin.py
+++ ylab_test_task/money_transfer/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+
+@admin.register(Transaction)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('from_user', 'to_user', 'amount', 'date')
+    list_filter = ('amount_currency', )
